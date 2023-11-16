b0d94394c04424ebcdfa91e8be27cc668df4d132
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:11:26 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,23 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-Storerobbery'
+description 'QBX_Storerobbery'
 repository 'https://github.com/Qbox-project/qbx_storerobbery'
 version '1.1.1'
 
 ui_page 'html/index.html'
 
 shared_scripts {
-    '@qbx_core/import.lua',
-    'configs/default.lua',
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
-    '@ox_lib/init.lua'
+    'configs/default.lua',
 }
 
 client_script 'client.lua'
 server_script 'server.lua'
-
-modules {'qbx_core:utils'}
 
 files {
     'html/index.html',
